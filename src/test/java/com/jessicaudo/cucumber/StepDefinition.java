ca7dcffa4a422d300f09555d24b507a08c41300a
Jessica <jessica.udo@mail.mcgill.ca>
2019-03-10 19:28:17 -0300

    StepDefinition.java started

---
+++ src/test/java/com/jessicaudo/cucumber/StepDefinition.java
@@ -0,0 +1,23 @@
+package com.jessicaudo.cucumber;
+
+import cucumber.annotation.en.And;
+import cucumber.annotation.en.Given;
+import cucumber.annotation.en.Then;
+import cucumber.annotation.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+
+public class StepDefinition {
+    // Variables
+    private WebDriver driver;
+    private final String PATH_TO_CHROME_DRIVER = "/Downloads/chromedriver";
+
+
+}
